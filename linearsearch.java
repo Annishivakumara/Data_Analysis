c6a32421349dd0c3c3b20ca0fb617f7972e5ebc4
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-12-02 13:19:02 +0530

    Create linearsearch.java

---
+++ linearsearch.java
@@ -0,0 +1,38 @@
+package search;
+import java.util.*;
+
+public class Linear {
+    int arr[] = new int[5];
+
+    public void read() {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < arr.length; i++) { 
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public void search(int data) {
+        boolean isFound = false; 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == data) {
+                System.out.println("Found");
+                isFound = true;
+                break;
+            }
+        }
+        
+        if (!isFound) {  
+            System.out.println("Not found");
+        }
+    }
+
+    public static void main(String[] args) {
+        Linear ls = new Linear();
+        ls.read();  
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter data to search:");
+        int data = sc.nextInt();
+        ls.search(data);  
+    }
+}

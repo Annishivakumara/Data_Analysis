47c9b7d40046d80efeb60f45ec3472a4754885b0
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-12-02 22:54:04 +0530

    Update selection.java

--- selection.java
+++ selection.java
@@ -1,27 +1,36 @@
-import java . util.*;
-public class selection{
-      int []  number={1 ,3 ,5,6, 8, 4,2};
-
-    public void bin(int low, int high , int data){
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(number[mid]==data){
-                System.out.println("Data found");
-                break;
-            }
-            else if(data>number[mid]){
-                mid=high-1;
-            }
-            else if(data>number[mid]){
-                mid=low+1;
-            }
-        
+import java.util.*;
+public class  selection {
+    int arr[] = new int[7];
+    public void read() {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < arr.length; i++) { 
+            arr[i] = sc.nextInt();
         }
     }
-    public static void main(String [] args){
-      selection s= new selection();
-      Scanner sc= new Scanner(System.in);
-      int data=sc.nextInt();
-      s.bin(0,4,data);
+   public void select(){
+    int min;
+    for(int i=0;i<arr.length-1;i++){
+        min=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
     }
-}
+     System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+   }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+          selection ls = new selection();
+         ls.read();  
+       ls.select();  
+    }
+    }
